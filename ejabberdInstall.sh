dfa93f8cfebe5c4d144a1f5787a39fdba672b112
Umar Samad <usamad@gmx.com>
2019-10-18 15:20:37 +0400

    Updated ejabberdInstall.sh
    Install Script now based on Debian Buster with Backports to Install ejabberd 19.09

--- ejabberdInstall.sh
+++ ejabberdInstall.sh
@@ -1,13 +1,14 @@
 sudo mkdir /var/lib/ejabberd
 sudo useradd -b /var/lib -s /bin/sh -r -M ejabberd
 
-sudo apt-get update && sudo apt-get upgrade -y &&sudo apt-get -t stretch-backports upgrade -y
-sudo apt-get -t stretch-backports install make autogen autoconf gcc git erlang libexpat1 libexpat1-dev libyaml-0-2 erlang openssl zlib1g zlib1g-dev imagemagick openssl libssl1.0-dev libyaml-dev unzip zip p7zip-full libsqlite3-0 libsqlite3-dev libpam0g libpam0g-dev libpam-modules libpam-modules-bin -y
+sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get -t buster-backports upgrade -y
+sudo apt-get -t buster-backports install make autogen autoconf gcc git erlang libexpat1 libexpat1-dev libyaml-0-2 erlang openssl.1 zlib1g zlib1g-dev imagemagick openssl libssl1.1 libssl-dev libyaml-dev unzip zip p7zip-full libsqlite3-0 lib$
 
 sudo chown -R ejabberd:ejabberd /var/lib/ejabberd
 
 git clone https://github.com/processone/ejabberd.git
 cd ejabberd
+sed -i 's+prefix = @prefix@+prefix =+g' Makefile.in
 ./autogen.sh
 ./configure --enable-user=ejabberd --enable-all
 make

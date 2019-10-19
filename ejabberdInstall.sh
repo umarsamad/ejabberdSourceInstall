d886aafaf71982fbe221f93eb2ef8a5ed0c9f7c6
Umar Samad <usamad@gmx.com>
2019-10-19 16:33:34 +0400

    Update ejabberdInstall.sh

--- ejabberdInstall.sh
+++ ejabberdInstall.sh
@@ -2,7 +2,9 @@ sudo mkdir /var/lib/ejabberd
 sudo useradd -b /var/lib -s /bin/sh -r -M ejabberd
 
 sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get -t buster-backports upgrade -y
-sudo apt-get -t buster-backports install make autogen autoconf gcc git erlang libexpat1 libexpat1-dev libyaml-0-2 erlang zlib1g zlib1g-dev imagemagick openssl libssl1.1 libssl-dev libyaml-dev unzip zip p7zip-full libsqlite3-0 libsqlite3-dev libpam0g libpam0g-dev libpam-modules libpam-modules-bin -y
+sudo apt-get -t buster-backports install make autogen autoconf gcc git erlang libexpat1 libexpat1-dev libyaml-0-2 erlang -y
+sudo apt-get -t buster-backports install zlib1g zlib1g-dev imagemagick openssl libssl1.1 libssl-dev libyaml-dev unzip zip -y
+sudo apt-get -t buster-backports install p7zip-full libsqlite3-0 libsqlite3-dev libpam0g libpam0g-dev libpam-modules libpam-modules-bin -y
 sudo chown -R ejabberd:ejabberd /var/lib/ejabberd
 
 git clone https://github.com/processone/ejabberd.git
